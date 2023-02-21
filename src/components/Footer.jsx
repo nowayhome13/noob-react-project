21d46e9b093a467ea33380e611b7bd95b3961367
Sumit Ghosh <80641668+Exynos03@users.noreply.github.com>
2023-02-21 22:11:47 +0530

    Noob React project #enjoyTheLearning

---
+++ src/components/Footer.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+function Footer() {
+  return (
+  <footer>
+    <div>
+      <h1>Test</h1>
+      <p>@all right reserved</p>
+    </div>
+
+    <div>
+      <h5>Follow Us</h5>
+      <div>
+        <a href="https://www.linkedin.com/in/sumit-ghosh-0a85a4195/" target={"blank"}>
+          linkedin
+        </a>
+        <a href="https://instagram.com/instagram.com/_ghosh.babu/" target={"blank"}>
+          Instagram
+        </a>
+        <a href="https://github.com/nowayhome13" target={"blank"}>
+          Github
+        </a>
+      </div>
+    </div>
+  </footer>
+  )
+}
+
+export default Footer
